bfb6bd9fd94236b5cd641f918e8276df6a7f28f2
ITAziret26 <azirettoichubekov263@gmail.com>
2022-01-20 17:15:20 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$('.sliders').slick({
+    slidesToShow: 4,
+    slidesToScrolls:1,
+    autoplay:true,
+    autoplaySpeed:1500,
+})
+
+
+$('.anims').slick({
+    slidesToShow: 3,
+    slidesToScrolls:2,
+    autoplay:true,
+    autoplaySpeed: 2000,
+
+})
